0c7f1e43fcad52381862babadb882c5c7ecfe024
sujithkumara <55655629+sujith-kumara@users.noreply.github.com>
2024-04-12 03:05:35 -0700

    Update data_config.py
    for ai server data path setting

--- data_config.py
+++ data_config.py
@@ -6,7 +6,7 @@ class DataConfig:
     def get_data_config(self, data_name):
         self.data_name = data_name
         if data_name == 'LEVIR':
-            self.root_dir = '/content/BIT_CD/LEVIR-CD/dataset'
+            self.root_dir = '/workspace/BIT_CD/LEVIR-CD/dataset'
         elif data_name == 'quick_start':
             self.root_dir = './samples/'
         else:
